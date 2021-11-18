98a94ac36d5b107e89e1012941070ff1e443315c
Kristiqn95 <55980434+Kristiqn95@users.noreply.github.com>
2021-11-18 11:54:07 +0200

    append fire emoji for all .hot.price

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,10 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+  
+  const hotPrices = document.querySelectorAll('.hot.price');
+  
+  for (const hotPrice of hotPrices) {
+    hotPrice.innerText = hotPrice.innerText + "🔥";
+  }
 });
